b82677ddd4818ea763cd7bfc8f0661fc5418e37e
LAPTOP-STTLPSP7\yanga <yanganoah404@gmail.com>
2023-02-28 18:26:45 +0800

    refactorization

--- Assets/Scripts/MeshFactory/ProtoCharacterFactory.cs
+++ Assets/Scripts/MeshFactory/ProtoCharacterFactory.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProtoCharacterFactor : MonoBehaviour
+public class ProtoCharacterFactory : MonoBehaviour
 {
     public CharacterType characterType;
     public CharacterMeshSet reference;
